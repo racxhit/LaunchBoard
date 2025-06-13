ee231ea526c1f64e48b5d9c6e585dbe173ac26a1
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2025-06-13 19:16:31 +0530

    Initial Xcode project setup

---
+++ LaunchBoard/LaunchBoard/LaunchBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchBoardApp.swift
+//  LaunchBoard
+//
+//  Created by Rachit Sharma on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LaunchBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
